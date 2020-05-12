95fef6d353b25a47138a55db9e705fcf1f65518d
Cezary Buliszak <cezary.buliszak@gmail.com>
2020-05-12 17:04:31 +0100

    Extract sprites to svg-sprites.svg on build.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
 module.exports = {
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
@@ -7,5 +9,14 @@ module.exports = {
     svgRule
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
+      .tap(() => ({
+        extract: true, // Enables SVG extraction when running in production mode
+        spriteFilename: () => 'svg-sprites.svg'
+      }))
+  },
+  configureWebpack: {
+    plugins: [
+      new SpriteLoaderPlugin() // Required if `extract` is set to `true`
+    ]
   }
 }
